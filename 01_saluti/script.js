06ce1d0f4de6a975662ba8dd2cebea3fc7c7a9ec
coluccifrancesco <coluccif2002@gmail.com>
2025-06-04 14:54:16 +0200

    Update greeting function to prompt for user name instead of using a hardcoded value

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -1,15 +1,18 @@
 // http://127.0.0.1:3000/01_saluti/index.html
 
-/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
+// Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]")
 
-const userName = 'Mario';
-
+const userName = prompt('Inserisci il tuo nome');
 
 // Dichiara la funzione qui.
+function sayHi (){
+    console.log('Ciao ' + userName + '!');
+    return
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+sayHi()
 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
