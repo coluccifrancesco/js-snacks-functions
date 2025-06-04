3ed688ee951e584e2e2a85a18b410fca4a91fa4a
coluccifrancesco <coluccif2002@gmail.com>
2025-06-04 15:35:45 +0200

    Update index.html title and fix script path; add showInit function to script.js

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -4,10 +4,24 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function showInit (){
+    let initLocator = []
 
+    for (let i = 0; i < names.length; i++){
+        let namesDisplay = names[i];
+
+        initLocator.push(namesDisplay.slice(0, 1));
+    }
+
+    return console.log(initLocator)
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+showInit()
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+
+
+
+//  http://127.0.0.1:3000/02_iniziali/index.html
