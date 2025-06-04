3bd44c7b3226790f624de0e8eb123a97efb38ac8
coluccifrancesco <coluccif2002@gmail.com>
2025-06-04 17:58:23 +0200

    Refactor vocalCheck function to improve logic and output of vowel counting, work on make the filter precise

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -2,12 +2,27 @@
 
 const word = 'javascript';
 
-
 // Dichiara la funzione qui.
+function vocalCheck (){
+    let characterStorage = [];
+    const vocali = ['a', 'e', 'i', 'o', 'u'];
 
+    for (let i = 0; i < word.length; i++){
+        
+        let charcterCounter = word[i];
+        console.log(charcterCounter);
+
+        if(charcterCounter.includes(vocali[i])) {
+            characterStorage.push(charcterCounter[i])
+        }
+    }
+
+    return console.log(characterStorage);
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+let vocalsInWord = vocalCheck();
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
+
+//  http://127.0.0.1:3000/04_conta_vocali/index.html
